197227b2a75956f26ad6185c9ac41a644b4e54b2
Taekyoung <>
2024-12-12 10:12:10 +0900

    모듈 수정 후

--- src/pages/sub302/Sub302.jsx
+++ src/pages/sub302/Sub302.jsx
@@ -1,47 +1,47 @@
 import React from 'react';
-import '../../styles/sub302.css'
+import styles from '../../styles/sub302.module.css';
 
 function Sub302(props) {
 
 
     return (
-        <div className='Sub302__content-container'>
-            <div className='Sub302__content-header'>
+        <div className={styles.Sub302__content_container}>
+            <div className={styles.Sub302__content_header}>
                 <h1>폐의약품 수거함 찾기</h1>
                 <p>원하는 지역에서의 녹지행사를 찾아보세요.</p>
             </div>
-            <div className='Sub302__search-FieldContainer'>
-                <div className='Sub302__mainField'>
+            <div className={styles.Sub302__search_FieldContainer}>
+                <div className={styles.Sub302__mainField}>
                     <p>지역 선택</p>
-                    <div className='Sub302__filterField'>
-                        <div className='Sub302__selects'>
-                            <select name="" id="" className='Sub302__select1'>
+                    <div className={styles.Sub302__filterField}>
+                        <div className={styles.Sub302__selects}>
+                            <select name="" id="" className={styles.Sub302__select1}>
                                 <option>서울특별시</option>
                                 <option>서울특별시</option>
                                 <option>서울특별시</option>
                             </select>
                         </div >
-                        <div className='Sub302__selects'>
-                            <select name="" id="" className='Sub302__select2'>
+                        <div className={styles.Sub302__selects}>
+                            <select name="" id="" className={styles.Sub302__select2}>
                                 <option>강남구</option>
                                 <option>강남구</option>
                                 <option>강남구</option>
                             </select>
                         </div>
-                        <div className='Sub302__selects'>
-                            <select name="" id="" className='Sub302__select3'>
+                        <div className={styles.Sub302__selects}>
+                            <select name="" id="" className={styles.Sub302__select3}>
                                 <option>역삼동</option>
                                 <option>역삼동</option>
                                 <option>역삼동</option>
                             </select>
                         </div>
                     </div>
-                    <div className='Sub302__searchbarField'>
+                    <div className={styles.Sub302__searchbarField}>
                             <p>검색어 검색</p>
-                        <div className='Sub302__searchField'>
-                            <div class="Sub302__search-container">
-                                <input type="text" class="Sub302__search-input" placeholder="상세주소로 검색하기" />
-                                <button class="Sub302__search-button">
+                        <div className={styles.Sub302__searchField}>
+                            <div className={styles.Sub302__search_container}>
+                                <input type="text" class={styles.Sub302__search_input} placeholder="상세주소로 검색하기" />
+                                <button class={styles.Sub302__search_button}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                         <circle cx="11" cy="11" r="8"></circle>
                                         <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
@@ -52,9 +52,9 @@ function Sub302(props) {
                     </div>
                 </div>
             </div>
-            <div className='Sub302__resultField'>
+            <div className={styles.Sub302__resultField}>
                 <p>"서울특별시 강북구 우이동’에서 ‘검색어’(으)로 검색한 결과는 총 0건 입니다."</p>
-                <div className='Sub302__map'></div>
+                <div className={styles.Sub302__map}></div>
             </div>
         </div>
     );
--- src/pages/sub401/Sub401.jsx
+++ src/pages/sub401/Sub401.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Pagination from '@mui/material/Pagination';
 import '../../styles/common.css';
 import { TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
-import '../../styles/sub401.css'
+import styles from '../../styles/sub401.module.css'
 import { Link } from 'react-router-dom';
 
 
@@ -15,99 +15,100 @@ function Sub401(props) {
 
 
     return (
-        <div className='Sub401__content-container'>
-            <div className='Sub401__content-header'>
+        <div className={styles.Sub401__content_container}>
+            <div className={styles.Sub401__content_header}>
                 <h1>공지사항</h1>
                 <p>약지기의 소식을 안내합니다.</p>
             </div>
 
-            <div className='Sub401__content-main'>
-                <div className='Sub401__searchbar-total'>
-                    <span className='Sub401__Total'>전체 29건</span>
-                    <div className='Sub401__searchbar-searchIcon'>
-                        <input className='Sub401__searchbar' type="text" />
-                        <span className="material-symbols-outlined Sub401__searchIcon">
+            <div className={styles.Sub401__content_main}>
+                <div className={styles.Sub401__searchbar_total}>
+                    <span className={styles.Sub401__Total}>전체 29건</span>
+                    <div className={styles.Sub401__searchbar_searchIcon}>
+                        <input className={styles.Sub401__searchbar} type="text" />
+                        <span className="material-symbols-outlined {styles.Sub401__searchIcon}">
                             search
                         </span>
                     </div>
                 </div>
                 <div>
-                    <table className='Sub401__table'>
+                    <table className={styles.Sub401__table}>
                         <thead>
-                            <tr className='Sub401__tr'>
-                                <td className='Sub401__cell1'><p>공지</p></td>
-                                <td className='Sub401__cell2'><p>소공원 정비사업 시행 안내</p></td>
-                                <td className='Sub401__cell3'>2024.00.00</td>
+                            <tr className={styles.Sub401__tr}>
+                                <td className={styles.Sub401__cell1}><p>공지</p></td>
+                                <td className={styles.Sub401__cell2}><p>소공원 정비사업 시행 안내</p></td>
+                                <td className={styles.Sub401__cell3}>2024.00.00</td>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr className='Sub401__tr'>
-                                <td className='Sub401__cell1'>10</td>
-                                <td className='Sub401__cell2'>
+                            <tr className={styles.Sub401__tr}>
+                                <td className={styles.Sub401__cell1}>10</td>
+                                <td className={styles.Sub401__cell2}>
                                     <p>소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 </p>
                                 </td>
-                                <td className='Sub401__cell3'>2024.00.00</td>
+                                <td className={styles.Sub401__cell3}>2024.00.00</td>
                             </tr>
-                            <tr className='Sub401__tr'>
-                                <td className='Sub401__cell1'>10</td>
-                                <td className='Sub401__cell2'><p>소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내 </p></td>
-                                <td className='Sub401__cell3'>2024.00.00</td>
+                            <tr className={styles.Sub401__tr}>
+                                <td className={styles.Sub401__cell1}>10</td>
+                                <td className={styles.Sub401__cell2}><p>소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내 </p></td>
+                                <td className={styles.Sub401__cell3}>2024.00.00</td>
                             </tr>
-                            <tr className='Sub401__tr'>
-                                <td className='Sub401__cell1'>10</td>
-                                <td className='Sub401__cell2'><p>소공원 정비사업 시행 안내 </p></td>
-                                <td className='Sub401__cell3'>2024.00.00</td>
+                            <tr className={styles.Sub401__tr}>
+                                <td className={styles.Sub401__cell1}>10</td>
+                                <td className={styles.Sub401__cell2}><p>소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내 </p></td>
+                                <td className={styles.Sub401__cell3}>2024.00.00</td>
                             </tr>
-                            <tr className='Sub401__tr'>
-                                <td className='Sub401__cell1'>10</td>
-                                <td className='Sub401__cell2'><p>소공원 정비사업 시행 안내 </p></td>
-                                <td className='Sub401__cell3'>2024.00.00</td>
+                            <tr className={styles.Sub401__tr}>
+                                <td className={styles.Sub401__cell1}>10</td>
+                                <td className={styles.Sub401__cell2}><p>소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내 </p></td>
+                                <td className={styles.Sub401__cell3}>2024.00.00</td>
                             </tr>
-                            <tr className='Sub401__tr'>
-                                <td className='Sub401__cell1'>10</td>
-                                <td className='Sub401__cell2'><p>소공원 정비사업 시행 안내 </p></td>
-                                <td className='Sub401__cell3'>2024.00.00</td>
+                            <tr className={styles.Sub401__tr}>
+                                <td className={styles.Sub401__cell1}>10</td>
+                                <td className={styles.Sub401__cell2}><p>소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내 </p></td>
+                                <td className={styles.Sub401__cell3}>2024.00.00</td>
                             </tr>
-                            <tr className='Sub401__tr'>
-                                <td className='Sub401__cell1'>10</td>
-                                <td className='Sub401__cell2'><p>소공원 정비사업 시행 안내 </p></td>
-                                <td className='Sub401__cell3'>2024.00.00</td>
+                            <tr className={styles.Sub401__tr}>
+                                <td className={styles.Sub401__cell1}>10</td>
+                                <td className={styles.Sub401__cell2}><p>소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내 </p></td>
+                                <td className={styles.Sub401__cell3}>2024.00.00</td>
                             </tr>
-                            <tr className='Sub401__tr'>
-                                <td className='Sub401__cell1'>10</td>
-                                <td className='Sub401__cell2'><p>소공원 정비사업 시행 안내 </p></td>
-                                <td className='Sub401__cell3'>2024.00.00</td>
+                            <tr className={styles.Sub401__tr}>
+                                <td className={styles.Sub401__cell1}>10</td>
+                                <td className={styles.Sub401__cell2}><p>소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내 </p></td>
+                                <td className={styles.Sub401__cell3}>2024.00.00</td>
                             </tr>
-                            <tr className='Sub401__tr'>
-                                <td className='Sub401__cell1'>10</td>
-                                <td className='Sub401__cell2'><p>소공원 정비사업 시행 안내 </p></td>
-                                <td className='Sub401__cell3'>2024.00.00</td>
+                            <tr className={styles.Sub401__tr}>
+                                <td className={styles.Sub401__cell1}>10</td>
+                                <td className={styles.Sub401__cell2}><p>소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내 </p></td>
+                                <td className={styles.Sub401__cell3}>2024.00.00</td>
                             </tr>
-                            <tr className='Sub401__tr'>
-                                <td className='Sub401__cell1'>10</td>
-                                <td className='Sub401__cell2'><p>소공원 정비사업 시행 안내 </p></td>
-                                <td className='Sub401__cell3'>2024.00.00</td>
+                            <tr className={styles.Sub401__tr}>
+                                <td className={styles.Sub401__cell1}>10</td>
+                                <td className={styles.Sub401__cell2}><p>소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내 </p></td>
+                                <td className={styles.Sub401__cell3}>2024.00.00</td>
                             </tr>
-                            <tr className='Sub401__tr'>
-                                <td className='Sub401__cell1'>10</td>
-                                <td className='Sub401__cell2'><p>소공원 정비사업 시행 안내 </p></td>
-                                <td className='Sub401__cell3'>2024.00.00</td>
+                            <tr className={styles.Sub401__tr}>
+                                <td className={styles.Sub401__cell1}>10</td>
+                                <td className={styles.Sub401__cell2}><p>소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내소공원 정비사업 시행 안내 </p></td>
+                                <td className={styles.Sub401__cell3}>2024.00.00</td>
                             </tr>
+                          
 
                         </tbody>
 
                     </table>
 
-                    <div className="Sub401__pagination">
-                        <Link className="Sub401__prev">&lt;&lt;</Link>
-                        <Link className="Sub401__prev">&lt;</Link>
-                        <Link className="Sub401__pagination_button">1</Link>
-                        <Link className="Sub401__pagination_button">2</Link>
-                        <Link className="Sub401__pagination_button">3</Link>
-                        <Link className="Sub401__pagination_button">4</Link>
-                        <Link className="Sub401__pagination_button">5</Link>
-                        <Link className="Sub401__next">&gt;</Link>
-                        <Link className="Sub401__next">&gt;&gt;</Link>
+                    <div className={styles.Sub401__pagination}>
+                        <Link className={styles.Sub401__prev}>&lt;&lt;</Link>
+                        <Link className={styles.Sub401__prev}>&lt;</Link>
+                        <Link className={styles.Sub401__pagination_button}>1</Link>
+                        <Link className={styles.Sub401__pagination_button}>2</Link>
+                        <Link className={styles.Sub401__pagination_button}>3</Link>
+                        <Link className={styles.Sub401__pagination_button}>4</Link>
+                        <Link className={styles.Sub401__pagination_button}>5</Link>
+                        <Link className={styles.Sub401__next}>&gt;</Link>
+                        <Link className={styles.Sub401__next}>&gt;&gt;</Link>
                     </div>
 
 
